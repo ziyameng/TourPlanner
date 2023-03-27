e174e66849408f3ac68c7d49154d5abee5f8f2c6
Thea Holtlund Jacobsen <thj1@st-andrews.ac.uk>
2023-03-27 18:39:05 +0100

    Create and add map API, and set up HTML document

---
+++ map.js
@@ -0,0 +1,7 @@
+// Module code: CS5003
+// Module: Masters Programming Projects
+// Matriculation number: 220024877, OTHER MATRICULATION NUMBERS
+// Creating a Holiday Planner
+
+// Initialise the map
+function initMap() {}
