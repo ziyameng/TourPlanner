3946eba5d3ee193b1a8ac39792e03805488bfa2c
Thea Holtlund Jacobsen <thj1@st-andrews.ac.uk>
2023-03-27 18:54:35 +0100

    Connect map through JS, add CSS

--- map.js
+++ map.js
@@ -4,4 +4,10 @@
 // Creating a Holiday Planner
 
 // Initialise the map
-function initMap() {}
+function initMap() {
+  var options = {
+    zoom: 8,
+    center: { lat: 56.3398, lng: -2.7967 }, // Positive for North, negative for West
+  };
+  var map = new google.maps.Map(document.getElementById("map"), options);
+}
