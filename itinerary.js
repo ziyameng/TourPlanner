dfd7b2d69770f2db1c4dadd554c75808e7757cd9
Miki Aoki <ma344@st-andrews.ac.uk>
2023-04-02 00:38:10 +0100

    add more functions to itinerary pg

--- itinerary.js
+++ itinerary.js
@@ -4,30 +4,31 @@ fetch("http://localhost:23843/user-locations")
     console.log(data);
 
     let table = document.getElementById("itineraryTable");
-    let row = table.insertRow();
-    let cell1 = row.insertCell(0);
-    let cell2 = row.insertCell(1);
-    let cell3 = row.insertCell(2);
-    let cell4 = row.insertCell(3);
-    let cell5 = row.insertCell(4);
-
-    /*
-    cell1.innerHTML = "hello";
-    cell2.innerHTML = "hello";
-    cell3.innerHTML = "hello";
-    cell4.innerHTML = "hello";
-    cell5.innerHTML = "<button>";*/
-
-    
-    data.forEach(activity => console.log(activity.activity, activity.coordinates));
+     
     for (let i in data){
-        console.log(data[i].activity);
+        let row = table.insertRow();
+        row.id = data[i].description;
+        let cell1 = row.insertCell(0);
+        let cell2 = row.insertCell(1);
+        let cell3 = row.insertCell(2);
+        let cell4 = row.insertCell(3);
+        let cell5 = row.insertCell(4);
+
         cell1.innerHTML = data[i].activity;
         cell2.innerHTML = data[i].name;
         cell3.innerHTML = data[i].description;
-        cell4.innerHTML = "hello";
-        cell5.innerHTML = "<button>Delete</button>";
+        cell4.innerHTML = data[i].date;
+        cell5.innerHTML = `<button id = ${data[i].activity} onclick="deleteActivity('${data[i].description}')">Delete</button>`;
+        console.log(data[i].activity);
     }
     
 })
+
+function deleteActivity(a){
+    let row = document.getElementById(a);
+    console.log("delete", a);
+    
+
+    row.remove();
+}
 
