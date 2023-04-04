d4cb4991fe24ceb3edc7e35af2759cdeadfb7017
Zia999 <92579255+Zia999@users.noreply.github.com>
2023-04-04 11:06:31 +0100

    Home page and Register page created based on authentication API

--- auth-api/server.js
+++ auth-api/server.js
@@ -1,18 +1,36 @@
 const express = require("express");
+const path = require("path");
 const cookieParser = require("cookie-parser");
 const connectDB = require("./config/database");
 const { adminAuth, userAuth } = require("./middleware/auth");
-connectDB();
 const app = express();
 require("dotenv").config();
 
 app.use(express.json());
-//Import Middleware
 app.use("/api/auth", require("./Auth/Route"));
 app.use(cookieParser());
 
 app.get("/admin", adminAuth, (req, res) => res.send("Admin Route"));
 app.get("/basic", userAuth, (req, res) => res.send("User Route"));
+
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "home.html"))
+);
+app.get("/register", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "register.html"))
+);
+app.get("/login", (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "login.html"))
+);
+app.get("/admin", adminAuth, (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "admin.html"))
+);
+app.get("/basic", userAuth, (req, res) =>
+  res.sendFile(path.join(__dirname, "views", "user.html"))
+);
+
+connectDB();
+
 const PORT = process.env.PORT;
 
 const server = app.listen(PORT, () =>
